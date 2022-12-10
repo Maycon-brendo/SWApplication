2b37ba55fa14910e6e2470c629851994dc36e71d
Maycon Brendo Santos Rocha <59679526+Maycon-brendo@users.noreply.github.com>
2022-12-09 23:15:04 -0300

    Delete SWApplication directory

--- SWApplication/app/src/main/java/com/example/swapplication/models/Banda.kt
+++
@@ -1,7 +0,0 @@
-package com.example.swapplication.models
-
-data class Banda(
-    val nomeBanda: String ="",
-    val generorBanda: String ="",
-    val anoBanda: String ="",
-)
--- SWApplication/app/src/main/java/com/example/swapplication/models/BandaComId.kt
+++
@@ -1,8 +0,0 @@
-package com.example.swapplication.models
-
-data class BandaComId (
-    val nomeBanda: String = "",
-    val generoBanda: String = "",
-    val formacaoBanda: String = "",
-    var id: String = ""
-)
